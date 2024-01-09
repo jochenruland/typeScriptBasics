1121bf66ba96346aa2e6384419565aead7d49d07
jochenruland <jochenhgruland@gmail.com>
2024-01-09 09:04:25 +0100

    ADD enums

--- 02BASICS/myEnums.ts
+++ 02BASICS/myEnums.ts
@@ -1,7 +1,8 @@
 // Enums
 
-// PART I
-// Enums exist to make some restrictions on the values which are allowed to be elected by the user
+// PART I - Enums exist to make some restrictions on the values which are allowed to be elected by the user 
+
+// Let's start with the following example which can be done better with Enums
 const AISLE = 0
 const MIDDLE = 1
 const WINDOW = 2
@@ -12,8 +13,8 @@ if (seat === 1) {
     // you make checks like this but what if someone changes the value of the constants? > Better with enums
 }
 
-// Better done with enums
-// enum SeatChoice {} -> this generally works but translates into a immediately invoked function (ifi) in javascript - means that it creates some complicated javascript code; therefore define as const variable
+// Now let's do the same with use of Enums
+// You can define an enum like this > "enum SeatChoice {}" -> this generally works but translates into an immediately invoked function (ifi) in javascript - means that it creates some complicated javascript code; therefore define as const variable
 const enum SeatChoice {
     AISLE = 10, // you can attibute initial values - setting the start value > follow up values will be incremented
     MIDDLE,
